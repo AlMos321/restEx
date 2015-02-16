e821938f098e9cfc5a1c8d90712d4bcde2568ad3
AlMos321 <alexmoss09@gmail.com>
2015-02-16 15:23:11 +0200

    first commit

---
+++ src/AppBundle/DataFixtures/ORM/LoadData.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace AppBundle\DataFixtures\ORM;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use AppBundle\Entity\User;
+class LoadData implements FixtureInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+        $user
+            ->setUsername('Pavel Durov')
+            ->setEmail('durov@gmail.com')
+            ->setPassword('p1232559d');
+        $manager->persist($user);
+        $manager->flush();
+    }
+}
